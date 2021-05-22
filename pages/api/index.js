fe492e9aaa3a7dba5631cc5791786d9a6610b8ef
winterlood <king199777@gmail.com>
2021-05-22 21:35:19 +0900

    [FIX] : fix static file path

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,7 +4,7 @@ import path from "path";
 var express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-// app.use(express.static("../../src/ejs"));
+app.use(express.static(__dirname + "../../src/ejs"));
 app.get("/api*", (req, res) => {
     const filePath = path.resolve(".", "public/svg/react_icon.svg");
     const svgBuffer = fs.readFileSync(filePath);
