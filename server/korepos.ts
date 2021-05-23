51fb80a911915fbf528da8062e0fca8a0e3b3771
winterlood <king199777@gmail.com>
2021-05-23 16:10:31 +0900

    Update korepos.ts

--- server/korepos.ts
+++ server/korepos.ts
@@ -13,9 +13,10 @@ export const koreposMedium =(repo:string,owner:string,theme:string)=>{
     <svg 
     xmlns:xlink="http://www.w3.org/1999/xlink" 
   xmlns="http://www.w3.org/2000/svg"
-    width="500"
+    width="501"
     height="150"
     viweBox="0 0 500 120"
+    fill="white"
     >
         <style>
            .repo{
@@ -23,13 +24,22 @@ export const koreposMedium =(repo:string,owner:string,theme:string)=>{
            }
            .label{ font: 400 13px 'Segoe UI', Ubuntu, Sans-Serif; fill: ${descriptColor}}
         </style>
-
-    <svg x="123" y="0" width="50%">
+        <rect
+        data-testid="card-bg"
+        x="0.5"
+        y="0.5"
+        rx="4.5"
+        height="99%"
+        width="500"
+        fill='${bgColor}'
+        stroke-opacity="1"
+        />
+    <svg x="123" y="0" width="48%">
     ${ko_divider}
     </svg>
 
-    <text class="label" text-anchor="middle" x="250" y="115">STARTED IN KOREA</text>
-    <text class="repo" text-anchor="middle" x="250" y="140">${owner}/${repo}</text>
+    <text class="label" text-anchor="middle" x="245" y="115">STARTED IN KOREA</text>
+    <text class="repo" text-anchor="middle" x="245" y="140">${owner}/${repo}</text>
 
   </svg>
             `   
